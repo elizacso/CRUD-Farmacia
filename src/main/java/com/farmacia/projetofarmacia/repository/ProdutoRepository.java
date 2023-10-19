92b8143b3aa3b678f3b603d5208639cd716d4576
elizacso <eelizareis@gmail.com>
2023-10-19 12:18:32 -0300

    configurando o projeto

--- src/main/java/com/farmacia/projetofarmacia/repository/ProdutoRepository.java
+++ src/main/java/com/farmacia/projetofarmacia/repository/ProdutoRepository.java
@@ -1,6 +1,9 @@
 package com.farmacia.projetofarmacia.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.farmacia.projetofarmacia.model.Produto;
@@ -8,5 +11,7 @@ import com.farmacia.projetofarmacia.model.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNameContainingIgnoreCase(@Param("nome") String nome);
 	
 }
