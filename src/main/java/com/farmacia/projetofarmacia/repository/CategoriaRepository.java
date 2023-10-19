abdfb4b50c772a80cc003018377f8284916f4af4
elizacso <eelizareis@gmail.com>
2023-10-19 12:57:11 -0300

    CRUD-Categoria

--- src/main/java/com/farmacia/projetofarmacia/repository/CategoriaRepository.java
+++ src/main/java/com/farmacia/projetofarmacia/repository/CategoriaRepository.java
@@ -1,16 +1,16 @@
 package com.farmacia.projetofarmacia.repository;
 
-
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.farmacia.projetofarmacia.model.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	
-
+	public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
+	
 
 }
